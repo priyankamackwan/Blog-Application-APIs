736c3ec8dfc91f0e7cbcc129a1b3bd6e8861f89a
Priyanka Mackwan <priyu08@gmail.com>
2023-06-23 20:19:29 +0530

    Update db.js

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,5 +2,5 @@ require('dotenv').config()
 const mongoose= require("mongoose");
 // *! change your user name and password form mongo atlas  while running on local machine
  module.exports= ()=>{
-    return mongoose.connect(`mongodb+srv://${process.env.MONGO_USER_NAME}:${process.env.MONGO_USER_PASSWORD}@Qcluster0.rgvsfz9.mongodb.net/Wp-blog-db?retryWrites=true&w=majority`)
+    return mongoose.connect(`mongodb+srv://${process.env.MONGO_USER_NAME}:${process.env.MONGO_USER_PASSWORD}@cluster0.iw17qvc.mongodb.net/Wp-blog-db?retryWrites=true&w=majority`)
  }
